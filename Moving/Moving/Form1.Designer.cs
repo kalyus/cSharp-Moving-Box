f93e70f3a82c8af79fb18f8e91e76c9c22f81620
tornadogarden <85133469+tornadogarden@users.noreply.github.com>
2021-06-30 22:51:16 -0500

    Add files via upload

---
+++ Moving/Moving/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace Moving
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.box = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.box)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // box
+            // 
+            this.box.BackColor = System.Drawing.Color.Red;
+            this.box.Location = new System.Drawing.Point(58, 51);
+            this.box.Name = "box";
+            this.box.Size = new System.Drawing.Size(50, 50);
+            this.box.TabIndex = 0;
+            this.box.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 625);
+            this.Controls.Add(this.box);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.box)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox box;
+    }
+}
+
---
+++ Moving/Moving/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Moving
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.D:
+                    if((box.Left + 10) < (this.Width - box.Width))
+                    {
+                        box.Left += 10;
+                    }
+                    
+                    break;
+
+                case Keys.A:
+                    if ((box.Left - 10) > 0)
+                    {
+                        box.Left -= 10;
+                    }
+
+                    break;
+
+                case Keys.W:
+                    if ((box.Top - 10) > 0)
+                    {
+                        box.Top -= 10;
+                    }
+
+                    break;
+
+                case Keys.S:
+                    if ((box.Top) < (this.Height - box.Height))
+                    {
+                        box.Top += 10;
+                    }
+
+                    break;
+            }
+
+        }
+    }
+}
